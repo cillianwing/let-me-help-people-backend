1f0edc23c6f68add70d437caaa0a8fe5b1455f17
Cillian Wing <cwing@zagmail.gonzaga.edu>
2021-01-12 15:53:35 -0600

    Added user foreign key to course migration.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :courses
   has_many :extra_curriculars
   has_many :work_experiences
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_12_210914) do
+ActiveRecord::Schema.define(version: 2021_01_12_215006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2021_01_12_210914) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
   create_table "extra_curriculars", force: :cascade do |t|
@@ -65,4 +67,5 @@ ActiveRecord::Schema.define(version: 2021_01_12_210914) do
     t.date "end"
   end
 
+  add_foreign_key "courses", "users"
 end
